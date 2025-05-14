1bb05850711e23888f0cf94e9c7127fe42d5d146
aghas004 <aden.agha04@gmail.com>
2025-05-14 18:50:18 +0500

    video#3 apple fall animation testing

--- survivalgame-platformer/scripts/apple_tree.gd
+++ survivalgame-platformer/scripts/apple_tree.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var state = "no apples"
 var player_in_area = false
+var apple = preload("res://scenes/apple_collectable.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,7 +19,7 @@ func _process(delta: float) -> void:
 		if player_in_area:
 			if Input.is_action_just_pressed("e"):
 				state = "no apples"
-				$growth_timer.start()
+				drop_apple()
 
 
 func _on_pickable_area_body_entered(body: Node2D) -> void:
@@ -33,3 +34,11 @@ func _on_pickable_area_body_exited(body: Node2D) -> void:
 func _on_growth_timer_timeout() -> void:
 	if state == "no apples":
 		state = "apple"
+		
+func drop_apple():
+	var apple_instance = apple.instantiate()
+	apple_instance.global_position = $Marker2D.global_position
+	get_parent().add_child(apple_instance)
+	
+	await  get_tree().create_timer(3).timeout
+	$growth_timer.start()
