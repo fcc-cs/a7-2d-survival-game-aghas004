66aea0cbe05b278f166e259f03bfda8146fe2711
aghas004 <aden.agha04@gmail.com>
2025-05-18 01:05:43 +0500

    video#8 enemy movement, health and loot completed

--- survivalgame-platformer/scripts/slime.gd
+++ survivalgame-platformer/scripts/slime.gd
@@ -5,6 +5,8 @@ var health = 100
 var dead = false
 var player_in_area = false
 var player
+@onready var slime = $slime_collectable
+@export var itemRes: InvItem
 
 func _ready() -> void:
 	dead = false
@@ -48,12 +50,22 @@ func death():
 	dead = true
 	$AnimatedSprite2D.play("death")
 	await get_tree().create_timer(1).timeout
+	drop_slime()
+	$AnimatedSprite2D.visible = false
+	$hitbox/CollisionShape2D.disabled = true
+	$detection_area/CollisionShape2D.disabled = true
+
+func drop_slime():
+	slime.visible = true
+	$slime_collect_area/CollisionShape2D.disabled = false
+	slime_collect()
+	
+func slime_collect():
+	await get_tree().create_timer(1.5).timeout
+	slime.visible = false
+	player.collect(itemRes)
 	queue_free()
 	
-	
-	
-	
-	
-	
-	
-	
+func _on_slime_collect_area_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player = body
