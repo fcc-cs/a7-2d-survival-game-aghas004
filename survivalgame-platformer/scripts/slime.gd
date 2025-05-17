8cb032419b9c4bb901c302c4aaa2c505a1e92f9d
aghas004 <aden.agha04@gmail.com>
2025-05-18 00:42:37 +0500

    video#8 enemy taking damage completed

--- survivalgame-platformer/scripts/slime.gd
+++ survivalgame-platformer/scripts/slime.gd
@@ -2,7 +2,6 @@ extends CharacterBody2D
 
 var speed = 50
 var health = 100
-var damage
 var dead = false
 var player_in_area = false
 var player
@@ -32,3 +31,29 @@ func _on_detection_area_body_entered(body: Node2D) -> void:
 func _on_detection_area_body_exited(body: Node2D) -> void:
 	if body.has_method("player"):
 		player_in_area = false
+
+
+func _on_hitbox_area_exited(area: Area2D) -> void:
+	var damage
+	if area.has_method("arrow_deal_damage"):
+		damage = 50
+		take_damage(damage)
+		
+func take_damage(damage):
+	health = health - damage
+	if health <= 0 and !dead:
+		death()
+		
+func death():
+	dead = true
+	$AnimatedSprite2D.play("death")
+	await get_tree().create_timer(1).timeout
+	queue_free()
+	
+	
+	
+	
+	
+	
+	
+	
